bd3eae33d70fece95b6d835cdf3aeb0acb1b1b0e
Benoît Claveau <benoit.claveau@gmail.com>
2020-01-06 15:34:24 +0100

    patch

--- src/pdf.js
+++ src/pdf.js
@@ -29,6 +29,8 @@ var pdfjsDisplayDisplayUtils = require("./display/display_utils.js");
 var pdfjsDisplaySVG = require("./display/svg.js");
 let pdfjsDisplayWorkerOptions = require("./display/worker_options.js");
 let pdfjsDisplayAPICompatibility = require("./display/api_compatibility.js");
+// PATCH
+let pdfjsCanvas = require("./display/canvas");
 
 if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
   const { isNodeJS } = require("./shared/is_node.js");
